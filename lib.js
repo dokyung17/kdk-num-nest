8b035c38a76acfd0a169df824a4ab881e15e5d3c
asdf <kdk7914@naver.com>
2024-10-31 21:09:25 +0900

    final

--- lib.js
+++ lib.js
@@ -1,24 +1,24 @@
 function avg(numbers) {
     let s = numbers.reduce((prev, curr) => prev + curr, 0);
     return s / numbers.length;
+}
+function prime(num) {
+    if (num<=1) return false;
+    for(let i=2;i<Math.sqrt(num);i++){
+        if(num%i==0)
+            return false;
     }
-    function prime(num) {
-        if (num<=1) return false;
-        for(let i=2;i<Math.sqrt(num);i++){
-            if(num%i==0)
-                return false;
-        }
-        return true;
+    return true;
+}
+function factorial(num) {
+    let fac=1;
+    for(let i=num;i>1;i--){
+        fac*=i;
     }
-    function factorial(num) {
-        let fac=1;
-        for(let i=num;i>1;i--){
-            fac*=i;
-        }
-    return fac;
-    }
-    module.exports = {
-    avg,
-    prime,
-    factorial
-    }
+return fac;
+}
+module.exports = {
+avg,
+prime,
+factorial
+}
--- lib.test.js
+++ lib.test.js
@@ -6,3 +6,16 @@ expect(lib.avg([3, 5, 7])).toBe(5);
 test("avg([-5, 5]) should be 0", () => {
 expect(lib.avg([-5, 5])).toBe(0);
 });
+test("prime([7]) should be true", () => {
+expect(lib.prime([7])).toBe(true);
+});
+test("prime([16]) should be false", () => {
+expect(lib.prime([16])).toBe(false);
+});
+test("factorial([7]) should be 5040", () => {
+expect(lib.factorial([7])).toBe(5040);
+});
+test("factorial([17]) should be 355687428096000", () => {
+expect(lib.factorial([17])).toBe(355687428096000);
+});
+
